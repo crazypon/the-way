24eeff58a21b508c66afe326d84b75a9c54e7a7c
crazypon <87004505+crazypon@users.noreply.github.com>
2021-07-10 08:46:33 +0500

    Update ClientServerforTishka.py

--- ClientServerforTishka.py
+++ ClientServerforTishka.py
@@ -1,17 +1,30 @@
 import socket
-import time
 
-Client_socket =socket.socket()
-Client_socket.connect(('localhost', 2222))
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "192.168.1.104"
+ADDR = (SERVER, PORT)
 
-while True:
-    msg = input('>>> ')
-    Client_socket.send(msg.encode('utf-8'))
-    # чтобы успеть получить сообщение
-    time.sleep(0.5)
-    data = Client_socket.recv(1024)
-    print(data.decode('utf-8'))
-    if msg == '!exit':
-        Client_socket.close()
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+send("Hello World!")
+input("[PRESS ENTER TO CONTINUE]")
+send("Hello Everyone!")
+input("[PRESS ENTER TO CONTINUE]")
+send("Hello Tishka and Alex!")
+
+send(DISCONNECT_MESSAGE)
 
 
