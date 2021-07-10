040800dc274326f820f9cb5b892e0c181db4dbb0
crazypon <87004505+crazypon@users.noreply.github.com>
2021-07-10 08:46:00 +0500

    Update ServerForTishka.py

--- ServerForTishka.py
+++ ServerForTishka.py
@@ -1,24 +1,44 @@
 import socket
-import select
+import threading
 
-Server = socket.socket()
-Server.bind(('localhost', 2222))
-Server.listen(5)
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
-ready_for_read = []
-ready_for_write = []
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
 
-while True:
-    conn, addr = Server.accept()
-    print(f'got connection from {addr}')
-    ready_for_read.append(conn)
-    ready_for_write.append(conn)
 
-    ready, writy, exy = select.select([ready_for_read], [ready_for_write], [ready_for_read])
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
 
-    for ser in ready:
-        data = ser.recv(1024)
-        print(data.decode('utf-8'))
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
 
-        for server in writy:
-            server.send(data.upper())
+            print(f"[{addr}] {msg}")
+            conn.send(msg.encode(FORMAT))
+
+    conn.close()
+
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+
+print("[STARTING] server is starting...")
+start()
