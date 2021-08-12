fe90469cef9e8f042390f540f81eeee35b9ea820
crazypon <87004505+crazypon@users.noreply.github.com>
2021-08-12 14:33:06 +0500

    Update ClientServerforTishka.py

--- ClientServerforTishka.py
+++ ClientServerforTishka.py
@@ -1,24 +1,26 @@
 import socket
 
 HEADER_SIZE = 10
-
 sock_serv = socket.socket()
-sock_serv.connect(('localhost', 444))
+sock_serv.connect(('localhost', 337))
+
+full_message = b''
 
 while True:
-    text = input('>>> ')
-    message = f'{len(text):<{HEADER_SIZE}}' + text
-    sock_serv.send(message.encode('utf-8'))
-
-    full_message = ''
-    new_message = True
     while True:
-        msg = sock_serv.recv(50)
-        if new_message:
-            msg_len = int(msg[:HEADER_SIZE])
-            new_message = False
-        full_message += msg.decode('utf-8')
+        text = input('>>> ')
+        msg = f'{len(text):<10}' + text
+        sock_serv.send(msg.encode('utf-8'))
+        break
 
-        if len(full_message) - HEADER_SIZE == msg_len:
-            print(full_message[HEADER_SIZE:])
-            new_message = True
+    data = sock_serv.recv(4096)
+    full_message += data
+    if len(full_message) >= 10:
+        msg_len = HEADER_SIZE + int(full_message[:HEADER_SIZE])
+        if len(full_message) >= msg_len:
+            payload = full_message[HEADER_SIZE:msg_len]
+            if payload == b'!stopserver':
+                sock_serv.close()
+                break
+            print(payload.decode('utf-8'))
+            full_message = full_message[msg_len:]
